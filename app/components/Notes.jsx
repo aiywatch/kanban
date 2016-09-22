7e6799b1b993ccad19aa1ca9df42748a6eeda780
aiywatch <aiycp35@gmail.com>
2016-09-21 23:22:37 -0700

    edit

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import Note from './Note';
+import Editable from './Editable';
 
-export default ({notes, onDelete = () => {}}) => (
+
+({notes, onDelete = () => {}}) => (
   <ul>{notes.map(({id, task}) => 
     <li key={id}>
-      <Note onDelete = {onDelete.bind(null, id)}
-      task={task} />
+
+
+      <Note>
+        <span>{task}</span>
+        <button onClick={onDelete.bind(null, id)}>x</button>
+      </Note>
     </li>
   )}</ul>
 );
+
+
+export default ({
+  notes,
+  onNoteClick=() => {}, onEdit=() => {}, onDelete=() => {}
+}) => (
+  <ul>{notes.map(({id, editing, task}) =>
+    <li key={id}>
+      <Note onClick={onNoteClick.bind(null, id)}>
+        <Editable
+          editing={editing}
+          value={task}
+          onEdit={onEdit.bind(null, id)} />
+        <button onClick={onDelete.bind(null, id)}>x</button>
+      </Note>
+    </li>
+  )}</ul>
+)
